f2562136d7c4b76a04458dde7a57d38dcdb432b7
Warren Yao <warrenyca905@gmail.com>
2019-05-23 00:26:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,13 +7,10 @@ def third_challenge
       :hero_friends => []
     },
     :capulet => {
-        :patriarch => {name: "Lord Capulet", age: "50"},
-        :matriarch => {name: "Lady Capulet", age: "51"},
-        :heroine => {name: "Juliet", age: "15", status: "alive"},
-        :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
-        ]
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => []
     }
   }
   
